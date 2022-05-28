e1c4888853e1b1918d6dfe30fdf46a318cee9af6
Alper Efe Şahin <efe.mate@gmail.com>
2022-05-28 12:54:11 +0300

    create core folder&file

---
+++ lib/presentation/core/app_widget.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: "Social Dating App",
+    );
+  }
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,13 +7,12 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:social_dating_app/main.dart';
+import 'package:social_dating_app/presentation/core/app_widget.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const AppWidget());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
