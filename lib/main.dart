befb7b3b9b21045520e9af093a4a7b224d638d2a
Alper Efe Şahin <efe.mate@gmail.com>
2022-05-28 14:15:44 +0300

    chore: firebase configurations

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:social_dating_app/firebase_options.dart';
 import 'package:social_dating_app/presentation/core/app_widget.dart';
 
-void main() => runApp(const AppWidget());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(const AppWidget());
+}
