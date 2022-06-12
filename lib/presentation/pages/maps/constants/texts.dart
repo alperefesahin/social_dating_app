fe20299d8c083b344c4c73d0f291c03dc54d886e
Alper Efe Şahin <efe.mate@gmail.com>
2022-06-12 20:14:45 +0300

    fix: no permission text

---
+++ lib/presentation/pages/maps/constants/texts.dart
@@ -0,0 +1,2 @@
+const String loadingOrPermission =
+    "If you gave a necessary permission, please wait. If you have no localization permission, go to settings, and give it.";
--- lib/presentation/pages/maps/maps_page.dart
+++ lib/presentation/pages/maps/maps_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:social_dating_app/domain/location/user_location_model.dart';
+import 'package:social_dating_app/presentation/pages/maps/constants/texts.dart';
 import 'package:social_dating_app/providers/maps/maps_provider.dart';
 
 class MapsPage extends ConsumerWidget {
@@ -14,7 +15,16 @@ class MapsPage extends ConsumerWidget {
     final usersMarkers = ref.read(mapsStateProvider).markerList;
 
     return currentUserLocation == UserLocationModel.empty()
-        ? const Center(child: Text("data"))
+        ? const Padding(
+          padding:  EdgeInsets.symmetric(horizontal: 20),
+          child:  Center(
+              child: Text(
+                loadingOrPermission,
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 22),
+              ),
+            ),
+        )
         : GoogleMap(
             initialCameraPosition: CameraPosition(
               target: LatLng(
