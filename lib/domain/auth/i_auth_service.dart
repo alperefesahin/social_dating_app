76edd017f71ddbdeeae2429712429ad3cb647282
Alper Efe Şahin <efe.mate@gmail.com>
2022-06-28 14:43:52 +0300

    refactor domain folder (files)

--- lib/domain/auth/i_auth_service.dart
+++ lib/domain/auth/i_auth_service.dart
@@ -6,15 +6,19 @@ import 'package:social_dating_app/domain/auth/auth_failure.dart';
 
 abstract class IAuthService {
   Stream<User?> get authStateChanges;
+
   Future<void> signOut();
+
   Stream<Either<AuthFailure, Tuple2<String, int?>>> signInWithPhoneNumber({
     required String phoneNumber,
     required Duration timeout,
     required int? resendToken,
   });
+
   Future<Either<AuthFailure, Unit>> verifySmsCode({
     required String smsCode,
     required String verificationId,
   });
+
   User? getCurrentUser();
 }
